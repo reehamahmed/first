6590b4462353bd910a3c42f1a96268f1f39e4cdb
reham <36538543+reehamahmed@users.noreply.github.com>
2018-02-22 22:28:05 +0200

    Add files via upload

---
+++ Q.2.ino
@@ -0,0 +1,45 @@
+const int led1=4;
+const int led2=5;
+const int led3=6;
+const int led4=7;
+
+void setup()
+{pinMode(led1,OUTPUT);
+
+pinMode(led2,OUTPUT);
+
+pinMode(led3,OUTPUT);
+
+pinMode(led4,OUTPUT);
+}
+void loop(){
+  digitalWrite(led1,HIGH);
+  delay(1000);
+
+  digitalWrite(led1,LOW);
+  
+  digitalWrite(led2,HIGH);
+  delay(1000);
+
+  digitalWrite(led2,LOW);
+  
+  digitalWrite(led3,HIGH);
+  delay(1000);
+  digitalWrite(led3,LOW);
+  
+  digitalWrite(led4,HIGH);
+  delay(1000);
+  digitalWrite(led4,LOW);
+
+  digitalWrite(led3,HIGH);
+  delay(1000);
+  digitalWrite(led3,LOW);
+
+  digitalWrite(led2,HIGH);
+  delay(1000);
+  digitalWrite(led2,LOW);
+  digitalWrite(led1,HIGH);
+  
+  
+  
+  }
